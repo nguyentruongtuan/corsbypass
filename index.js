79cfe4184376d422460b1b3f95954bab0925e688
Tuan Truong <tuannt@devblock.net>
2019-05-31 11:42:25 +0700

    Remove origin header from server log request data

--- index.js
+++ index.js
@@ -10,9 +10,18 @@ app.use ('/', (req, res) => {
     const url = req.url;
     const api = process.env.API;
 
-    console.log(api + url);
+    console.log(api + url, req.rawHeaders, req.params);
 
-    req.pipe(request({url: api + url})).pipe(res);
+    req.on('data', (buffer) => {
+        console.log(buffer.toString())
+    })
+
+    const _request = request({url: api + url});
+    _request.on('response', (_res) => {
+        _res.headers['access-control-allow-origin'] = "*"
+    })
+    req.pipe(_request)
+    .pipe(res);
 });
 
 app.listen(5000, () => {
